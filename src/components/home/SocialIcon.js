fe0a36a30c24a3e7d6a1cea3eba2cf0937b9a5c4
Jacob Adamson <jakeladamson0@gmail.com>
2022-10-11 23:42:34 -0600

    icon dev

---
+++ src/components/home/SocialIcon.js
@@ -0,0 +1,12 @@
+//import
+
+function SocialIcon(props) {
+  const { link, icon, label } = props;
+  return (
+    <a target="_blank" aria-label={label} rel="noopener noreferrer" href={link}>
+      <i className={icon} aria-hidden="true" />
+    </a>
+  );
+}
+
+export default SocialIcon;
