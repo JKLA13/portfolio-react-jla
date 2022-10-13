689810fabb38cbd2b3462d606d159ab2c7b65746
Jacob Adamson <jakeladamson0@gmail.com>
2022-10-12 20:35:05 -0600

    dev PortfolioBlock, IconLink comps

---
+++ src/components/portfolio/IconLink.js
@@ -0,0 +1,14 @@
+//import
+
+import React from "react";
+
+function IconLink(props) {
+  const { link, title, icon } = props;
+  return (
+    <a href={link} target={"_blank"} rel="noopener noreferrer">
+      <i className={icon} /> {title}
+    </a>
+  );
+}
+
+export default IconLink;
